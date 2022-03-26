861484d7be540bb9e023ab1106329bd2ed40e63c
Aradhya roy <aradhya11711@gmail.com>
2022-03-27 00:06:41 +0530

    divisibleby2

---
+++ divisibleby2.js
@@ -0,0 +1,23 @@
+ 
+    for (let i=2; i<=20; i=i+2) {
+        console.log(i)
+    }
+
+for(let i=0; i<10; i++)
+{
+    console.log(`2 * ${i+1} = ${(i+1)*2}`)
+}
+for(let i=0; i<=9; i=i+2)
+{
+    console.log(`5 * ${i+1} = ${(i+1)*5}`)
+}
+let a=5;
+let b=6;
+ let sum=0
+ console.log(sum)
+
+ for(let i=1; i<=10; i++)
+ {
+    sum=sum+i
+ }
+ console.log(sum)
--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,3 @@
 console.log('hello world')
+// cd C:
+// cd js
